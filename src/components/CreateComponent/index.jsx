02052b69422410bc1efda29ba85dcfee6973c339
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-10-16 18:50:17 -0700

    copied CanvasArea.jsx

--- src/components/CreateComponent/index.jsx
+++ src/components/CreateComponent/index.jsx
@@ -1,20 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom/dist";
 import { useAuth } from "../../hooks/useAuth";
-
 import CanvasArea from "./CanvasArea";
 import Sidebar from "./Sidebar";
 
+
 const CreateComponent = () => {
+    const [userAction, setUserAction] = useState("addText");
     const navigate = useNavigate();
     const { userAuth } = useAuth();
     useEffect(() => {
         if(!userAuth)navigate("/")
     })
+
     return (
         <div className="flex gap-x-4">
-            <CanvasArea />
-            <Sidebar />
+            <CanvasArea userAction={userAction} setUserAction={setUserAction} />
+            <Sidebar userAction={userAction} setUserAction={setUserAction} />
         </div>
     );
 }
