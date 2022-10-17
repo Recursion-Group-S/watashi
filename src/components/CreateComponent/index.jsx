3de067a6a368343d2bb3ec7e83f30163538e28a6
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-10-16 19:04:34 -0700

    delete useAuth

--- src/components/CreateComponent/index.jsx
+++ src/components/CreateComponent/index.jsx
@@ -1,17 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom/dist";
-import { useAuth } from "../../hooks/useAuth";
 import CanvasArea from "./CanvasArea";
 import Sidebar from "./Sidebar";
 
 
 const CreateComponent = () => {
     const [userAction, setUserAction] = useState("addText");
-    const navigate = useNavigate();
-    const { userAuth } = useAuth();
-    useEffect(() => {
-        if(!userAuth)navigate("/")
-    })
 
     return (
         <div className="flex gap-x-4">
