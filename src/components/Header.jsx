bbf9d0b1745d1c9e5302f2a1601d7e6f006dac86
satojin <renzuoteng142@gmail.com>
2022-10-22 20:03:27 +0900

    change user_icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,34 @@
+import { useAtomValue } from "jotai";
 import React from "react";
+import { authUserAtom } from "../atoms/authUser";
 
 const Header = () => {
-    return (
-        <header>
-            <div className="mx-auto max-w-screen-xl px-4 py-2 sm:py-4 sm:px-6 lg:px-8">
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <div className="text-center sm:text-left">
-                        <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-                            Watashi
-                        </h1>
-                    </div>
+  const userAuth = useAtomValue(authUserAtom);
 
-                    <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
-                        <a href="/gallery">Gallery</a>
-                        <img className="w-10 h-10 rounded-full" src="https://i.pravatar.cc/300" alt="Rounded avatar"></img>
-                    </div>
-                </div>
-            </div>
-        </header>
+  return (
+    <header>
+      <div className="mx-auto max-w-screen-xl px-4 py-2 sm:py-4 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="text-center sm:text-left">
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              Watashi
+            </h1>
+          </div>
 
-    );
-}
+          <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+            <a href="/gallery">Gallery</a>
+            {userAuth ? (
+              <img
+                className="w-10 h-10 rounded-full"
+                src={userAuth.photoURL}
+                alt="Rounded avatar"
+              />
+            ) : null}
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/db/user.js
+++ src/db/user.js
@@ -3,7 +3,6 @@ import { db } from "../client/firebase"
 
 export const postUser = (uid, name, email, iconUrl) => {
   const userCollectionRef = doc(db, "users", uid);
-  console.log(userCollectionRef)
   setDoc(userCollectionRef, {
     name: name,
     email: email,
