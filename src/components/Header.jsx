d55fa9c976a497df898307e36ff3a661cbeb6a98
hayato0309 <hayato.pluto.4538@gmail.com>
2022-10-27 21:16:47 +0900

    feat/change_landing_page_design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useAtomValue } from 'jotai'
+import { authUserAtom } from "../atoms/authUser";
 
 const Header = () => {
+    const authUser = useAtomValue(authUserAtom);
+
     return (
         <header>
             <div className="mx-auto max-w-screen-xl px-4 py-2 sm:py-4 sm:px-6 lg:px-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <div className="text-center sm:text-left">
-                        <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
-                            Watashi
-                        </h1>
+                        <img className="w-36" src="/images/watashi_header_logo.png" alt="watashi_logo" />
                     </div>
 
-                    <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center">
+                    {console.log(authUserAtom === null)}
+                    <div className="mt-4 flex flex-col gap-4 sm:mt-0 sm:flex-row sm:items-center" style={{ display: authUser ? "" : "none" }}>
                         <a href="/gallery">Gallery</a>
                         <img className="w-10 h-10 rounded-full" src="https://i.pravatar.cc/300" alt="Rounded avatar"></img>
                     </div>
