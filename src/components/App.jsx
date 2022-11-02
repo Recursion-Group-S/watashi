373afff48e29633dd437eb83c01a223100e2b1cb
satojin <renzuoteng142@gmail.com>
2022-11-02 19:28:33 +0900

    install react-query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "jotai";
 import React from "react";
 import { authUserAtom } from "../atoms/authUser";
@@ -5,11 +6,15 @@ import "../index.css";
 
 import { RouterConfig } from "./RouterConfig";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
+    <QueryClientProvider client={queryClient}>
     <Provider scope={Symbol()} initialValues={[[authUserAtom, null]]}>
       <RouterConfig />
     </Provider>
+    </QueryClientProvider>
   );
 };
 
