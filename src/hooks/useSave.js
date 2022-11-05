4a1c57fbc5f7596363f591f95784f8d934bc0c14
Takeshi Hashimoto <htakeshi0614@gmail.com>
2022-11-05 11:27:06 -0700

    use navigate after posting map

--- src/hooks/useSave.js
+++ src/hooks/useSave.js
@@ -2,12 +2,14 @@ import { useAtom, useAtomValue } from "jotai"
 import { backgroundImageAtom, canvasItemsAtom, stageRefAtom } from "./../atoms/ComponentAtom";
 import { currentMapAtom } from "../atoms/CurrentMapAtom";
 import { postMap } from "../db/map";
+import { useNavigate } from "react-router-dom";
 
 export const useSave = () => {
     const [canvasItems, setCanvasItems] = useAtom(canvasItemsAtom);
     const [stageRef] = useAtom(stageRefAtom);
     const currentMap = useAtomValue(currentMapAtom)
     const [backgroundImage, setBackgroundImage] = useAtom(backgroundImageAtom)
+    const navigate = useNavigate();
 
     const saveMap = () => {
         let data = stageRef.current.toDataURL();
@@ -27,7 +29,7 @@ export const useSave = () => {
             backgroundImage: backgroundImage.src,
             createdAt: currentMap.createdAt
         }
-        postMap(newMap);
+        postMap(newMap).then(() => navigate('/gallery'))
         setBackgroundImage(new Image());
     }
     return { saveMap }
